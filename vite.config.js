6af00a4550631464cf9a66010c230aacd464d135
b0000m <55938676+0000inp@users.noreply.github.com>
2025-01-20 20:29:20 +0700

    dist

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/my-portfolio-site/',
+  assetsInclude: ['**/*.JPG'],
   plugins: [vue()],
 })
