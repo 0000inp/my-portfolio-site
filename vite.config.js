fc60cfc64b09538efb1a83c8f516900de23a36c8
0000inp <boomredxd@gmail.com>
2025-02-10 17:03:22 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/portfolio/',
-  assetsInclude: ['**/*.JPG'],
+  assetsInclude: ['**/*.JPG', '**/*.mov'],
   plugins: [vue()],
 })
